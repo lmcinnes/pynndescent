2c28150eb89ff962850a9d849d901a733139832d
Leland McInnes <leland.mcinnes@gmail.com>
2018-02-24 21:13:36 -0500

    Update pynndescent_.py

--- pynndescent/pynndescent_.py
+++ pynndescent/pynndescent_.py
@@ -41,11 +41,8 @@ def make_initialisations(dist, dist_args):
     @numba.njit()
     def init_from_tree(tree, data, query_points, heap, rng_state):
         for i in range(query_points.shape[0]):
-            indices = search_flat_tree(query_points[i],
-                                       tree.hyperplanes,
-                                       tree.offsets,
-                                       tree.children,
-                                       tree.indices,
+            indices = search_flat_tree(query_points[i], tree.hyperplanes,
+                                       tree.offsets, tree.children, tree.indices,
                                        rng_state)
 
             for j in range(indices.shape[0]):
@@ -53,6 +50,7 @@ def make_initialisations(dist, dist_args):
                     continue
                 d = dist(data[indices[j]], query_points[i], *dist_args)
                 heap_push(heap, i, d, indices[j], 1)
+
         return
 
     return init_from_random, init_from_tree
@@ -70,7 +68,7 @@ def initialise_search(forest, data, query_points, n_neighbors,
 
 
 def make_initialized_nnd_search(dist, dist_args):
-    #@numba.njit(parallel=True)
+    @numba.njit(parallel=True)
     def initialized_nnd_search(data,
                                knn_graph,
                                initialization,
@@ -78,19 +76,19 @@ def make_initialized_nnd_search(dist, dist_args):
 
         for i in range(query_points.shape[0]):
 
-            point = query_points[i]
-
             while True:
+
                 # Find smallest flagged vertex
                 vertex = smallest_flagged(initialization, i)
+
                 if vertex == -1:
                     break
                 candidates = knn_graph[vertex]
                 for j in range(candidates.shape[0]):
                     if candidates[j] == vertex or candidates[j] == -1:
                         continue
-                    d = dist(data[candidates[j]], point, *dist_args)
-                    heap_push(initialization, i, d, candidates[j], 1)
+                    d = dist(data[candidates[j]], query_points[i], *dist_args)
+                    pushed = heap_push(initialization, i, d, candidates[j], 1)
 
         return initialization
 
@@ -180,7 +178,6 @@ def make_nn_descent(dist, dist_args):
         return deheap_sort(current_graph)
 
     return nn_descent
-
 
 class NNDescent(object):
     def __init__(self, data,
