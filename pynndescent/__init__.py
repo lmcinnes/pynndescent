a6d011a6a3e1ffcea3232a37e5a11079382320a8
James Melville <jlmelville@gmail.com>
2019-05-15 20:39:04 -0700

    Move imports, as demanded by pylint

--- pynndescent/__init__.py
+++ pynndescent/__init__.py
@@ -1,10 +1,8 @@
+import pkg_resources
+import numba
 from .pynndescent_ import NNDescent, PyNNDescentTransformer
 
 # Workaround: https://github.com/numba/numba/issues/3341
-import numba
-
 numba.config.THREADING_LAYER = "workqueue"
-
-import pkg_resources
 
 __version__ = pkg_resources.get_distribution("pynndescent").version
