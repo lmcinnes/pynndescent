972881f4b446b7706254e90edf87ced90fe4ab66
Leland McInnes <leland.mcinnes@gmail.com>
2019-06-11 20:18:06 -0400

    Version bump for patching of sparse support

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 configuration = {
     "name": "pynndescent",
-    "version": "0.3.0",
+    "version": "0.3.1",
     "description": "Nearest Neighbor Descent",
     "long_description": readme(),
     "classifiers": [
