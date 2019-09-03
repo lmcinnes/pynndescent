1ac5ca606b3e55f89824f9f38d01894eea0e14c6
Isaac Virshup <ivirshup@gmail.com>
2019-09-03 12:43:45 +1000

    Test equivalence of NNDescent vs PyNNDescentTranformer

--- pynndescent/tests/test_pynndescent_.py
+++ pynndescent/tests/test_pynndescent_.py
@@ -154,6 +154,37 @@ def test_sparse_nn_descent_query_accuracy():
     )
 
 
+def test_transformer_equivalence():
+    N_NEIGHBORS = 15
+    QUEUE_SIZE = 5.0
+    train = nn_data[:400]
+    test = nn_data[:200]
+
+    nnd = NNDescent(
+        data=train,
+        n_neighbors=N_NEIGHBORS,
+        random_state=42
+    )
+    indices, dists = nnd.query(test, k=N_NEIGHBORS, queue_size=QUEUE_SIZE)
+    sort_idx = np.argsort(indices, axis=1)
+    indices_sorted = np.vstack(
+         [indices[i, sort_idx[i]] for i in range(sort_idx.shape[0])]
+    )
+    dists_sorted = np.vstack(
+        [dists[i, sort_idx[i]] for i in range(sort_idx.shape[0])]
+    )
+
+    transformer = PyNNDescentTransformer(
+        n_neighbors=N_NEIGHBORS,
+        search_queue_size=QUEUE_SIZE,
+        random_state=42
+    ).fit(train)
+    Xt = transformer.transform(test).sorted_indices()
+
+    assert np.all(Xt.indices == indices_sorted.flat)
+    assert np.allclose(Xt.data, dists_sorted.flat)
+
+
 def test_random_state_none():
     knn_indices, _ = NNDescent(
         nn_data, "euclidean", {}, 10, random_state=None
