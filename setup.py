00e89fcec7ae08e09061a19987749ac391679ac4
Leland McInnes <leland.mcinnes@gmail.com>
2019-09-07 14:27:03 -0400

    Time for a new patch release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 configuration = {
     "name": "pynndescent",
-    "version": "0.3.1",
+    "version": "0.3.2",
     "description": "Nearest Neighbor Descent",
     "long_description": readme(),
     "classifiers": [
