cb3322ecdb023308549332c299544365d41431be
Leland McInnes <leland.mcinnes@gmail.com>
2020-09-12 13:22:09 -0400

    We have arrived!

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 configuration = {
     "name": "pynndescent",
-    "version": "0.5.0rc2",
+    "version": "0.5.0",
     "description": "Nearest Neighbor Descent",
     "long_description": readme(),
     "classifiers": [
