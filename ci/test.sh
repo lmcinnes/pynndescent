6f3e249298f4da6f50d767b4446092eededed62b
leriomaggio <valerio.maggio@gmail.com>
2021-02-12 14:11:20 +0000

    Fixed Coverage command for Travis

--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@ set -e
 COVERAGE_ARGS=""
 if [[ "${COVERAGE}" == "true" ]]; then
     export NUMBA_DISABLE_JIT=1
-    COVERAGE_ARGS="--cov=${MODULE}"
+    COVERAGE_ARGS="--junitxml=junit/test-results.xml --cov=pynndescent/ --cov-report=xml --cov-report=html"
 fi
 
-pytest --show-capture=no -v --disable-warnings ${COVERAGE_ARGS} ${MODULE} && black --diff ${MODULE} && black --check ${MODULE}
+pytest ${MODULE} --show-capture=no -v --disable-warnings ${COVERAGE_ARGS} && black --diff ${MODULE} && black --check ${MODULE}
