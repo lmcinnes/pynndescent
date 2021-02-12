5643c1647ad94c6aa12e1986504a58c807476f6b
leriomaggio <valerio.maggio@gmail.com>
2021-02-12 13:38:05 +0000

    PyTest integration and Py3.8 pipeline - dropped nose support

--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@ set -e
 COVERAGE_ARGS=""
 if [[ "${COVERAGE}" == "true" ]]; then
     export NUMBA_DISABLE_JIT=1
-    COVERAGE_ARGS="--with-coverage --cover-package=${MODULE}"
+    COVERAGE_ARGS="--cov=${MODULE}"
 fi
 
-nosetests -s -v ${COVERAGE_ARGS} ${MODULE} && black --diff ${MODULE} && black --check ${MODULE}
+pytest --show-capture=no -v --disable-warnings ${COVERAGE_ARGS} ${MODULE} && black --diff ${MODULE} && black --check ${MODULE}
