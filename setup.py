ea3d1fef30098c8832e2a8046d944bd8d8ebfa41
Leland McInnes <leland.mcinnes@gmail.com>
2022-10-31 13:16:52 -0400

    Release version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 configuration = {
     "name": "pynndescent",
-    "version": "0.5.7",
+    "version": "0.5.8",
     "description": "Nearest Neighbor Descent",
     "long_description": readme(),
     "classifiers": [
