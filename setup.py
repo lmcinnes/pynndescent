9392f2368b33d3b39aef0fcd90a0323132c82cea
gclendenning <64431984+gclendenning@users.noreply.github.com>
2025-10-11 09:26:57 +0100

    Convert to pyproject.toml and build module

--- setup.py
+++ setup.py
@@ -1,55 +1,5 @@
 from setuptools import setup
 
-
-def readme():
-    with open("README.rst") as readme_file:
-        return readme_file.read()
-
-
-configuration = {
-    "name": "pynndescent",
-    "version": "0.5.13",
-    "description": "Nearest Neighbor Descent",
-    "long_description": readme(),
-    "classifiers": [
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved",
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-        "Topic :: Scientific/Engineering",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Operating System :: Unix",
-        "Operating System :: MacOS",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-    ],
-    "keywords": "nearest neighbor, knn, ANN",
-    "url": "http://github.com/lmcinnes/pynndescent",
-    "author": "Leland McInnes",
-    "author_email": "leland.mcinnes@gmail.com",
-    "maintainer": "Leland McInnes",
-    "maintainer_email": "leland.mcinnes@gmail.com",
-    "license": "BSD",
-    "packages": ["pynndescent"],
-    "install_requires": [
-        "scikit-learn >= 0.18",
-        "scipy >= 1.0",
-        "numba >= 0.51.2",
-        "llvmlite >= 0.30",
-        "joblib >= 0.11",
-        'importlib-metadata >= 4.8.1; python_version < "3.8"',
-    ],
-    "ext_modules": [],
-    "cmdclass": {},
-    "test_suite": "nose.collector",
-    "tests_require": ["nose"],
-    "data_files": (),
-    "zip_safe": False,
-}
-
-setup(**configuration)
+# All package metadata is now defined in pyproject.toml as per PEP 621.
+# This setup() call is retained for compatibility or other tooling purposes.
+setup()
